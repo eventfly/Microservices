11b692aa60ddda59b6e7ac9ec706e748bfec8605
jawadulkabir <jawaduk15@gmail.com>
2022-08-09 20:01:01 +0600

    [frontend-org] eventfeed new quiz modal added

--- frontend-org/src/screens/EventFeed.js
+++ frontend-org/src/screens/EventFeed.js
@@ -1,7 +1,17 @@
+import { Button } from "react-bootstrap";
 import EventSidebar from "../components/EventSidebar";
 import "../styles/EventFeed.css"
+import { useState } from "react";
+import PopupModal from "../components/PopupModal";
+import SubscriptionModal from "../components/Subscription/SubscriptionModal";
+
+import Quiz from "../components/Feed/Quiz/Quiz";
 
 const EventFeed = () => {
+
+    const [quizModalShow, setQuizModalShow] = useState(false);
+    const [pollModalShow, setPollModalShow] = useState(false);
+
     return ( 
 
         <>
@@ -19,6 +29,26 @@ const EventFeed = () => {
                 {/* </div>
             
             </div> */}
+
+            
+
+            <Button variant="contained" color="primary" onClick={ () => setQuizModalShow(true)}> Add quiz</Button>
+            <Button variant="contained" color="primary" onClick={ () => setPollModalShow(true)}> Add poll</Button>    
+
+            <PopupModal
+                show={quizModalShow}
+                onHide={() => setQuizModalShow(false)}
+                header="QUIZ"
+                bodyComponent={<Quiz onHide={() => setQuizModalShow(false)}/>}
+                size="lg"
+            />
+
+            <PopupModal
+                show={pollModalShow}
+                onHide={() => setPollModalShow(false)}
+                header="Poll"
+                bodyComponent={<SubscriptionModal />}
+            />
         
         </>
 
