5765df650a8ddb101b73bee2f5baf4436c5a1158
TanzimAzadNishan <1705074@ugrad.cse.buet.ac.bd>
2022-08-09 21:56:33 +0600

    [events] remove-staff route added

--- events/src/routes/remove-staff.ts
+++ events/src/routes/remove-staff.ts
@@ -3,12 +3,35 @@ import { requireAuth } from '../middlewares/require-auth';
 import express, { Request, Response } from 'express';
 import { errorHandler } from '../middlewares/error-handler';
 import { Event } from '../models/event';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
-router.get('/api/event/:id/remove-staff', [], currentUser, requireAuth, errorHandler, async (req: Request, res:Response) => {
-    const events = await Event.find({ });
-    res.status(200).send(events);
+router.post('/api/event/:id/remove-staff', [], currentUser, requireAuth, errorHandler, async (req: Request, res:Response) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    
+    const event = await Event.findOneAndUpdate({"ref_id": req.params.id}, {
+        $pull: {
+          staffs: {
+            ref_id: req.body.ref_id
+          }
+        }
+      }, {
+        new: true,
+        runValidators: true
+    })
+
+    natsWrapper.client.publish('staff:removed', JSON.stringify(
+        {
+            eventId: req.params.id, 
+            ref_id: req.body.ref_id
+        }
+    ), () => {
+        console.log('Removed Staff published')
+    })
+
+    res.status(201).send({ event })
 });
 
 export { router as removeStaffRouter };
