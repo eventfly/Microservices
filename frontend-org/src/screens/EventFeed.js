a2a5295a6bf455c7ad40bc650bc1d106559a1fd2
jawadulkabir <jawaduk15@gmail.com>
2022-08-15 21:49:25 +0600

    [frontend-org] post-comment added

--- frontend-org/src/screens/EventFeed.js
+++ frontend-org/src/screens/EventFeed.js
@@ -11,7 +11,11 @@ import PollModal from "../components/Feed/Poll/PollModal";
 import QuizModal from "../components/Feed/Quiz/QuizModal";
 import PostModal from "../components/Feed/Post/PostModal";
 
+import Post from "../components/Feed/Post/Post";
+
 const EventFeed = () => {
+
+    const mess = 'sdfkjfsdjfnajkebfkjsdfasdf a sdufhak ad asduifhaeuhffsdjfnajkebfkjsdfasdf a sdufhak ad asduifhaeuhffsdjfnajkebfkjsdfasdf a sdufhak ad asduifhaeuhffsdjfnajkebfkjsdfasdf a sdufhak ad asduifhaeuhff sdjfnajkebfkjsdfasdf a sdufhak ad asduifhae uhffsdjfnajkeb fkjsdfasdf a sdufhak ad asduifhaeuhffsdjfnajkebfkjsdfasdf a sdufhak ad asduifhaeuhfa kdfcicch9eurf a efifh389 fu uiefh8e'
 
 
     return ( 
@@ -25,7 +29,15 @@ const EventFeed = () => {
             <QuizModal />
             <PollModal />
             <PostModal />
-            
+
+            <div className='feed-container'>
+
+                <Post message={mess} username='jawad' timestamp='14/8/2022' image='https://i.redd.it/xmr50tmyqjh91.jpg'/>
+                <Post message={mess} username='jawad' timestamp='14/8/2022' image='https://i.redd.it/4diyr90qddg91.jpg'/>
+                <Post message={mess} username='jawad' timestamp='14/8/2022' image='https://i.redd.it/d6kybhyceqh91.jpg'/>
+                <Post message={mess} username='jawad' timestamp='14/8/2022'image='https://i.redd.it/tfur3uql6fh91.jpg'/>
+
+            </div>
           
         
         </>
